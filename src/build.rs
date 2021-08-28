1ed3d4c1c7f836f7dfe9b4b4e6544a29b84db479
David M <PieKing1215@users.noreply.github.com>
2021-08-28 04:25:42 -0400

    Initial commit

---
+++ src/build.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let src = [
+        "pxtone/pxtone.cpp",
+        "ogg/bitwise.c",
+        "ogg/framing.c",
+        "ogg/vorbisfile.c",
+        "ogg/lib/info.c",
+        "ogg/lib/block.c",
+        "ogg/lib/synthesis.c",
+        "ogg/lib/sharedbook.c",
+        "ogg/lib/codebook.c",
+        "ogg/lib/psy.c",
+        "ogg/lib/floor0.c",
+        "ogg/lib/floor1.c",
+        "ogg/lib/registry.c",
+        "ogg/lib/mdct.c",
+        "ogg/lib/envelope.c",
+        "ogg/lib/smallft.c",
+        "ogg/lib/bitrate.c",
+        "ogg/lib/window.c",
+        "ogg/lib/lpc.c",
+        "ogg/lib/res0.c",
+        "ogg/lib/lsp.c",
+        "ogg/lib/mapping0.c",
+    ];
+
+    let mut builder = cc::Build::new();
+    let build = builder
+        .cpp(true)
+        .files(src.iter())
+        .include("ogg")
+        .include("pxtone")
+        .define("pxINCLUDE_OGGVORBIS", None);
+        
+    build.compile("pxtone");
+}
