10a0255879311dc80aaf13110de65d222df8d10d
David M <PieKing1215@users.noreply.github.com>
2022-09-08 01:25:24 -0400

    Make one of the tasks in build.rs not in a thread

--- src/build.rs
+++ src/build.rs
@@ -45,41 +45,38 @@ fn main() {
         bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
     });
 
-    let vorbis = thread::spawn(|| {
-        let src = [
-            "ogg/bitwise.c",
-            "ogg/framing.c",
-            "ogg/vorbisfile.c",
-            "ogg/lib/info.c",
-            "ogg/lib/block.c",
-            "ogg/lib/synthesis.c",
-            "ogg/lib/sharedbook.c",
-            "ogg/lib/codebook.c",
-            "ogg/lib/psy.c",
-            "ogg/lib/floor0.c",
-            "ogg/lib/floor1.c",
-            "ogg/lib/registry.c",
-            "ogg/lib/mdct.c",
-            "ogg/lib/envelope.c",
-            "ogg/lib/smallft.c",
-            "ogg/lib/bitrate.c",
-            "ogg/lib/window.c",
-            "ogg/lib/lpc.c",
-            "ogg/lib/res0.c",
-            "ogg/lib/lsp.c",
-            "ogg/lib/mapping0.c",
-        ];
+    let src = [
+        "ogg/bitwise.c",
+        "ogg/framing.c",
+        "ogg/vorbisfile.c",
+        "ogg/lib/info.c",
+        "ogg/lib/block.c",
+        "ogg/lib/synthesis.c",
+        "ogg/lib/sharedbook.c",
+        "ogg/lib/codebook.c",
+        "ogg/lib/psy.c",
+        "ogg/lib/floor0.c",
+        "ogg/lib/floor1.c",
+        "ogg/lib/registry.c",
+        "ogg/lib/mdct.c",
+        "ogg/lib/envelope.c",
+        "ogg/lib/smallft.c",
+        "ogg/lib/bitrate.c",
+        "ogg/lib/window.c",
+        "ogg/lib/lpc.c",
+        "ogg/lib/res0.c",
+        "ogg/lib/lsp.c",
+        "ogg/lib/mapping0.c",
+    ];
 
-        let mut builder = cc::Build::new();
-        let build = builder
-            .files(src.iter())
-            .include("ogg")
-            .flag_if_supported("-fpermissive");
-        
-        build.compile("vorbis");
-    });
+    let mut builder = cc::Build::new();
+    let build = builder
+        .files(src.iter())
+        .include("ogg")
+        .flag_if_supported("-fpermissive");
+    
+    build.compile("vorbis");
 
     bindings.join().unwrap();
-    vorbis.join().unwrap();
     pxtone.join().unwrap();
 }
